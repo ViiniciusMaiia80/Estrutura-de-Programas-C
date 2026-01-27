3c015066ba95d627bb8fc3bf6585c10260aaa297
vinicius maia <viniciusmaia140@gmail.com>
2026-01-27 14:59:36 -0400

    Técnicas de recuo V:1.0.0

---
+++ 3.32-Outro problema do else pendurado.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+int main(void){
+	int x=5,y=8;
+
+	printf("Saida da letra A:\n");
+	if(y==8)
+	if(x==5)
+	printf("@@@@@\n");
+	else
+	printf("#####\n");
+	printf("$$$$$\n");
+	printf("&&&&&\n");
+
+	printf("Saida da letra B:\n");
+	if(y==8){
+	if(x==5){
+	printf("@@@@@\n");
+	}else{
+	printf("#####\n");
+	printf("$$$$$\n");
+	printf("&&&&&\n");
+	}
+	
+	printf("Saida da letra C:\n");
+	if(y==8){
+	if(x==5)
+	printf("@@@@@\n");
+	}else{
+	printf("#####\n");
+	printf("$$$$$\n");
+	}
+	printf("&&&&&\n");
+	}
+
+	printf("Saida da letra D:\n");
+	y=7;
+
+	if(y==8){
+	if(x==5)
+	printf("@@@@@\n");
+	}else{
+	printf("#####\n");
+	printf("$$$$$\n");
+	printf("&&&&&\n");
+	}
+	return 0;
+}
