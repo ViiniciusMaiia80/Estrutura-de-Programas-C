b58634899c6c86be9196d5c4c7b526f1ff0d7b00
vinicius maia <viniciusmaia140@gmail.com>
2026-02-03 00:28:02 -0400

    V: 1.0.0

---
+++ 3.41-Multiplos de 2 com looping infinito.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void){
+	for(int i=1;i>=0;i++){
+		printf("%d\n",i*2);
+	}
+	return 0;
+}
+
+/*Após executar o programa, aparentemente nada acontece;
+porém ao interromper a execução, o programa começa a gerar
+números de 2 multiplamente*/
